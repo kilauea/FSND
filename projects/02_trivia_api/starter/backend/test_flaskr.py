c783108ddcf30e57551b413dbf95c33a1d8af739
Arturo Crespo de la Viña <acrespo@MacBook-Pro-de-Arturo.local>
2020-04-26 17:04:26 +0200

    Adding node_modules to .gitignore. Creating test cases for categories/ and questions/ end points.

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -8,32 +8,67 @@ from models import setup_db, Question, Category
 
 
 class TriviaTestCase(unittest.TestCase):
-    """This class represents the trivia test case"""
+  """This class represents the trivia test case"""
 
-    def setUp(self):
-        """Define test variables and initialize app."""
-        self.app = create_app()
-        self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
+  def setUp(self):
+    """Define test variables and initialize app."""
+    self.app = create_app()
+    self.client = self.app.test_client
+    self.database_name = "trivia_test"
+    self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+    setup_db(self.app, self.database_path)
 
-        # binds the app to the current context
-        with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
-            # create all tables
-            self.db.create_all()
-    
-    def tearDown(self):
-        """Executed after reach test"""
-        pass
+    # binds the app to the current context
+    with self.app.app_context():
+      self.db = SQLAlchemy()
+      self.db.init_app(self.app)
+      # create all tables
+      self.db.create_all()
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+  def tearDown(self):
+    """Executed after reach test"""
+    pass
 
+  """
+  TODO
+  Write at least one test for each test for successful operation and for expected errors.
+  """
+  def test_get_categories(self):
+    res = self.client().get('/categories')
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertTrue(len(data['categories']))
+    self.assertTrue(data['total_categories'])
+
+  def test_404_get_categories_beyond_valid_page(self):
+    res = self.client().get('/categories?page=0')
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 404)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'], 'resource not found')
+
+  def test_get_questions(self):
+    res = self.client().get('/questions')
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertTrue(len(data['questions']))
+    self.assertTrue(data['total_questions'])
+    self.assertTrue(isinstance(data['categories'], dict))
+    self.assertTrue(len(data['categories']))
+    self.assertTrue(data['current_category'])
+
+  def test_404_get_questions_beyond_valid_page(self):
+    res = self.client().get('/questions?page=0')
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 404)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'], 'resource not found')
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
